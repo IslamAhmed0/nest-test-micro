7647ac47e0f17af17675a7b5dcd1aca35a128f5a
IslamAhmed0 <ea43533@gmail.com>
2022-12-11 22:55:34 +0200

    edit

--- dist/config/typeorm.config.d.ts
+++
@@ -1,6 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-import { TypeOrmModuleAsyncOptions, TypeOrmModuleOptions } from '@nestjs/typeorm';
-export default class TypeOrmConfig {
-    static getOrmConfig(configservice: ConfigService): TypeOrmModuleOptions;
-}
-export declare const typeOrmConfigAsync: TypeOrmModuleAsyncOptions;
